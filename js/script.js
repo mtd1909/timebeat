071accde9282bf9300ed176aeffbca3b9704eece
Mai Thành Đạt <datmt@timebird.org>
2022-08-11 17:28:25 +0700

    update UI

--- js/script.js
+++ js/script.js
@@ -137,4 +137,20 @@ $(document).ready(function() {
     }, 1000 );
     e.preventDefault();
   });
+
+  $( '.header__logo' ).on( 'click', function(e){
+    var href = $(this).attr( 'href' );
+    $( 'html, body' ).animate({
+      scrollTop: $( href ).offset().top
+    }, 1000 );
+    e.preventDefault();
+  });
+
+  $( '.footer__img' ).on( 'click', function(e){
+    var href = $(this).attr( 'href' );
+    $( 'html, body' ).animate({
+      scrollTop: $( href ).offset().top
+    }, 1000 );
+    e.preventDefault();
+  });
 })
