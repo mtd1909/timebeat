abd6aebd32180b262dd2a5595ad433bb75e70196
Mai Thành Đạt <datmt@timebird.org>
2022-08-12 09:51:58 +0700

    update

--- js/script.js
+++ js/script.js
@@ -41,10 +41,10 @@ $(document).ready(function() {
     loop: true,
     items: 1,
     responsive:{
-      768: {
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-      }
+      // 768: {
+      //   animateOut: 'fadeOut',
+      //   animateIn: 'fadeIn',
+      // }
     }
   })
   
